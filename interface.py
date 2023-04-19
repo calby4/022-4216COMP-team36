87cdd9cf9486e6ebb4d1229ba783a89a90151406
Lewis <csmlasha@ljmu.ac.uk>
2023-04-19 04:58:57 +0100

    Added a way to filter the data using the filter b- uttons. made it more in line with OOP practices. needs some more commenting on it tho to explain how the program works.

--- interface.py
+++ interface.py
@@ -23,25 +23,66 @@ dataList = ['title','year','synopsis','critic_score','people_score','rating','ge
 class App(ctk.CTk):
 
     """
+    function getColumnsForTable
+    params self
+
+    This function generates the columns for the tree Table. It works by first checking if all values are set to 0,
+    if so, return the default columnList (dataList) otherwise, go through the list and add the correct columns
+    (where the value in checkboxData == 1)
+    """
+
+    def getColumnsForTable(self):
+        
+        #if all values are equal to 0, then display all columns
+        total = -1
+        for i in range(0, len(self.checkboxData)):
+            total += self.checkboxData[i].get()
+
+        if total == -1:
+            return self.dataList
+        else:
+            newColumnList = []
+            for i in range(0, len(self.checkboxData)):
+                if self.checkboxData[i].get() == 1:
+                    newColumnList.append(self.dataList[i])
+            
+            return newColumnList
+
+    """
     function generateTableData
     params  self
 
     This function generates the data that is displayed in self.tree, or rather displays the movies and their
     corresponding attributes.
     """
-    def generateTableData(self, dataFile):
+    def generateTableData(self, dataFile, columns):
 
         for item in self.tree.get_children():
             self.tree.delete(item)
 
-        for i in self.dataList:
-            self.tree.column(i,width=120,anchor='center')
-            self.tree.heading(i,text=str(i))
+        for i in columns:
+            self.tree.heading(i, text=str(i))
+            self.tree.column(i, width=120, stretch=False)
         
         for i in range(1, len(dataFile.iloc[1:])):
-            newdataList = list(dataFile.iloc[i].loc[self.dataList])
+            newdataList = list(dataFile.iloc[i].loc[columns])
             self.tree.insert(parent='',index='end', text='', values=newdataList)
             newdataList.clear()   
+
+    """
+    function generateTable
+    params self
+
+    Generates a treeview named self.tree. 
+    """
+
+    def generateTable(self):
+
+        columns = self.getColumnsForTable()
+        self.tree = ttk.Treeview(self,columns=columns,show='headings')
+        self.tree.grid(row=7, column = 1, columnspan=5, rowspan = 3)
+        return columns
+
 
     """
     function searchFunction
@@ -53,21 +94,68 @@ class App(ctk.CTk):
     def searchFunction(self):
         
         searchEntryData = self.searchEntry.get()
+        columns = self.getColumnsForTable()
         if searchEntryData == "":
-            self.generateTableData(self.dataFile)
+            self.generateTableData(self.dataFile, columns)
         else:
             newDataFrame = self.dataFile.loc[self.dataFile['title'].str.contains(searchEntryData, case=False)]
-            self.generateTableData(newDataFrame)
+            self.generateTableData(newDataFrame, columns)
             self.searchEntry.delete(first_index=0 ,last_index=tk.END)
-                
+
+    def fillCheckboxData(self):
+        for i in range(0, 12):
+            self.checkboxData.append(tk.IntVar(value=0))
+    """
+    function refreshFunction
+    params self
+
+    This function clears all filters from the table by destroying the table and re-creating it.
+    """
+    def refreshFunction(self):
+        
+        #Clear all filters
+        self.choice1.deselect()
+        self.choice2.deselect()
+        self.choice3.deselect()
+        self.choice4.deselect()
+        self.choice5.deselect()
+        self.choice6.deselect()
+        self.choice7.deselect()
+        self.choice8.deselect()
+        self.choice9.deselect()
+        self.choice10.deselect()
+        self.choice11.deselect()
+        self.choice12.deselect()
+
+        #destroy tree and regenerate as new.
+        self.tree.destroy()
+        columns = self.generateTable()
+        self.generateTableData(self.dataFile, columns)
+
+    def updateFunction(self):
+        
+        #destroy tree and regenerate as new.
+        self.tree.destroy()
+        columns = self.generateTable()
+        self.generateTableData(self.dataFile, columns)
+
+
+    """
+    function init
+    params self, *args, **kwargs
+
+    This function initialises our tk application. 
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs) 
         # Title of the APP
         self.title("Rotten Tomatoes Top Movies Data Searcher")
         self.geometry(f"{appWidth}x{appHeight}")
+        self.dataList = ['title','year','synopsis','critic_score','people_score','rating','genre','original_language','director','producer','runtime','link']
+        self.checkboxData = []
 
         #Initialises an empty filter list and creates a pandas DataFrame object from our CSV
-        self.filterList = []
         self.dataFile = pd.read_csv("rotten_tomatoes_top_movies.csv")
 
         # Search Label
@@ -80,49 +168,53 @@ class App(ctk.CTk):
         
         self.searchButton = ctk.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"), text="Search", command=self.searchFunction)
         self.searchButton.grid(row = 0, column = 4)
+
+        #Refresh Table button.
+        self.refreshTableButton = ctk.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"), text="Clear Filters", command=self.refreshFunction)
+        self.refreshTableButton.grid(row = 0, column = 5)
  
         # Filter choice label
         self.choiceLabel = ctk.CTkLabel(self, text="Select Filters")
         self.choiceLabel.grid(row=3, column=0, padx=20, pady=20, sticky="ew")
  
+        self.fillCheckboxData()
+
         # Filter check boxes
-        self.checkbox1Var = ctk.IntVar(value=0)
-        self.checkboxVar = ctk.IntVar(value=0)
          
-        self.choice1 = ctk.CTkCheckBox(self, text="Title", variable=self.checkbox1Var, onvalue=1, offvalue=0)
+        self.choice1 = ctk.CTkCheckBox(self, text="Title", variable=self.checkboxData[0], onvalue=1, offvalue=0, command=self.updateFunction)
         self.choice1.grid(row=3, column=1, padx=20, pady=20, sticky="ew")
  
-        self.choice2 = ctk.CTkCheckBox(self, text="Year", variable=self.checkboxVar, onvalue=1, offvalue=0)                              
+        self.choice2 = ctk.CTkCheckBox(self, text="Year", variable=self.checkboxData[1], onvalue=1, offvalue=0, command=self.updateFunction)                              
         self.choice2.grid(row=3, column=2, padx=20, pady=20, sticky="ew")
         
-        self.choice3 = ctk.CTkCheckBox(self, text="Synopsis", variable=self.checkboxVar, onvalue=1, offvalue=0)                              
+        self.choice3 = ctk.CTkCheckBox(self, text="Synopsis", variable=self.checkboxData[2], onvalue=1, offvalue=0, command=self.updateFunction)                              
         self.choice3.grid(row=3, column=3, padx=20, pady=20, sticky="ew")
         
-        self.choice4 = ctk.CTkCheckBox(self, text="Critic Score", variable=self.checkboxVar, onvalue=1, offvalue=0)                              
+        self.choice4 = ctk.CTkCheckBox(self, text="Critic Score", variable=self.checkboxData[3], onvalue=1, offvalue=0, command=self.updateFunction)                              
         self.choice4.grid(row=3, column=4, padx=20, pady=20, sticky="ew")
         
-        self.choice5 = ctk.CTkCheckBox(self, text="People Score", variable=self.checkboxVar, onvalue=1, offvalue=0)
+        self.choice5 = ctk.CTkCheckBox(self, text="People Score", variable=self.checkboxData[4], onvalue=1, offvalue=0, command=self.updateFunction) 
         self.choice5.grid(row=4, column=1, padx=20, pady=20, sticky="ew")
  
-        self.choice6 = ctk.CTkCheckBox(self, text="Rating", variable=self.checkboxVar, onvalue=1, offvalue=0)                              
+        self.choice6 = ctk.CTkCheckBox(self, text="Rating", variable=self.checkboxData[5], onvalue=1, offvalue=0, command=self.updateFunction)                               
         self.choice6.grid(row=4, column=2, padx=20, pady=20, sticky="ew")
         
-        self.choice7 = ctk.CTkCheckBox(self, text="Genre", variable=self.checkboxVar,onvalue=1, offvalue=0)                              
+        self.choice7 = ctk.CTkCheckBox(self, text="Genre", variable=self.checkboxData[6],onvalue=1, offvalue=0, command=self.updateFunction)                              
         self.choice7.grid(row=4, column=3, padx=20, pady=20, sticky="ew")
         
-        self.choice8 = ctk.CTkCheckBox(self, text="Language", variable=self.checkboxVar, onvalue=1, offvalue=0)                              
+        self.choice8 = ctk.CTkCheckBox(self, text="Language", variable=self.checkboxData[7], onvalue=1, offvalue=0, command=self.updateFunction)                              
         self.choice8.grid(row=4, column=4, padx=20, pady=20, sticky="ew")
         
-        self.choice9 = ctk.CTkCheckBox(self, text="Director", variable=self.checkboxVar, onvalue=1, offvalue=0)                              
+        self.choice9 = ctk.CTkCheckBox(self, text="Director", variable=self.checkboxData[8], onvalue=1, offvalue=0, command=self.updateFunction)                              
         self.choice9.grid(row=5, column=1, padx=20, pady=20, sticky="ew")
         
-        self.choice10 = ctk.CTkCheckBox(self, text="Producer", variable=self.checkboxVar, onvalue=1, offvalue=0)                              
+        self.choice10 = ctk.CTkCheckBox(self, text="Producer", variable=self.checkboxData[9], onvalue=1, offvalue=0, command=self.updateFunction)                             
         self.choice10.grid(row=5, column=2, padx=20, pady=20, sticky="ew")
         
-        self.choice11 = ctk.CTkCheckBox(self, text="Runtime", variable=self.checkboxVar, onvalue=1, offvalue=0)                              
+        self.choice11 = ctk.CTkCheckBox(self, text="Runtime", variable=self.checkboxData[10], onvalue=1, offvalue=0, command=self.updateFunction)                              
         self.choice11.grid(row=5, column=3, padx=20, pady=20, sticky="ew")
         
-        self.choice12 = ctk.CTkCheckBox(self, text="Link", variable=self.checkboxVar, onvalue=1, offvalue=0)                              
+        self.choice12 = ctk.CTkCheckBox(self, text="Link", variable=self.checkboxData[11], onvalue=1, offvalue=0, command=self.updateFunction)                               
         self.choice12.grid(row=5, column=4, padx=20, pady=20, sticky="ew")
          
         # Sort Label
@@ -134,10 +226,8 @@ class App(ctk.CTk):
         self.sortOptionMenu.grid(row=6, column=1, padx=20, pady=20,  sticky="ew")
         
         #Creates the grid used to display the data.
-        self.dataList = ['title','year','synopsis','critic_score','people_score','rating','genre','original_language','director','producer','runtime','link']
-        self.tree = ttk.Treeview(self,columns=self.dataList,show='headings')
-        self.tree.grid(row=7, column = 1, columnspan=5, rowspan = 3)     
-        self.generateTableData(self.dataFile)
+        columns = self.generateTable()
+        self.generateTableData(self.dataFile, columns)
 
 
 if __name__ == "__main__":
